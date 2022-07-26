c6332383e4f4c97e8f850f691162e3e588980ab0
Arqui9897 <arqui9897@gmail.com>
2022-07-26 15:37:18 -0500

    testA

--- resources/views/plantilla/footer.blade.php
+++ resources/views/plantilla/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row align-items-center flex-row-reverse">
             <div class="col-md-12 col-sm-12 text-center">
-                Copyright © <span id="year"></span> <a href="javascript:void(0)">Sash</a>. Designed with <span
+                Copyright © <span id="year"></span> <a href="javascript:void(0)">SBUC</a>. Designed with <span
                     class="fa fa-heart text-danger"></span> by <a href="javascript:void(0)"> Spruko </a> All rights reserved.
             </div>
         </div>
